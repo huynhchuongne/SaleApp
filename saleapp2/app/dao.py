0ea6a1fec2aa10cc3c542c9be103c2165ec32e93
Chuong <nguyenchuong.nncnhn@gmail.com>
2023-10-30 11:59:38 +0700

    init

---
+++ saleapp2/app/dao.py
@@ -0,0 +1,59 @@
+def get_categories():
+    return [{
+        "id": "1",
+        "name": "Mobile"
+    },{
+        "id": 2,
+        "name": "Tablet"
+    }]
+def get_products():
+    products = [{
+        "id": 1,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    },{
+        "id": 2,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    },{
+        "id": 3,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    },{
+        "id": 4,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    },{
+        "id": 5,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    },{
+        "id": 6,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    },{
+        "id": 7,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    },{
+        "id": 8,
+        "name": "Iphone 13",
+        "price": 200000,
+        "image": "https://trangthienlong.com.vn/product/iphone-13-pro-max-1tb-99/?attribute_pa_mau-sac=xanh-duong&gad=1&gclid=EAIaIQobChMIzaqfqfWcggMVvRF7Bx1q2QFoEAQYAiABEgIUmfD_BwE",
+        "category_id":1
+    }]
+    return products
